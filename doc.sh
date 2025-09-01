edf69e7ee0354ef815b91688f0f26c2793351ffe
Gypsophila1912 <noma.19.anko@gmail.com>
2025-09-01 20:07:18 +0900

    add doc.shを追加しました。

---
+++ doc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 使用する前に権限を設定してください
+# chmod +x doc.sh
+
+#実行コマンド一覧
+#使い方：./doc.sh
+#実行：./doc.sh start
+#落とす：./doc.sh down
+
+
+if [ "$1" == "start" ]; then
+    ./vendor/bin/sail npm install
+    ./vendor/bin/sail npm run dev
+    echo "アプリを立ち上げました。"
+
+elif [ "$1" == "down" ]; then
+    ./vendor/bin/sail down
+    echo "Docker コンテナを停止しました。"
+else
+    echo "使い方: ./doc.sh start|down"
+fi
